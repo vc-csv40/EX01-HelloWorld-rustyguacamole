0395dae1ce6060e04fb02a4cb19485a779a38fbe
rustyguacamole <59850086+rustyguacamole@users.noreply.github.com>
2020-01-27 14:20:28 -0800

    Update HelloWorld.java

--- src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.vcccd.vc.csv40;
 
 public class HelloWorld {
         public static void main(String[] args) {
-            System.out.println("Hello World!");
+            System.out.println("Hello, World!");
         }
     }
